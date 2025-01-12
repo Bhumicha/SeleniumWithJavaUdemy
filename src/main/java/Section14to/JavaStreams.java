1036e073aec66bc690dc502ab018433669153c52
Bhumika Shrestha <66097955+Bhumicha@users.noreply.github.com>
2025-01-12 20:50:08 +0545

    javastreams

--- src/main/java/Section14to/JavaStreams.java
+++ src/main/java/Section14to/JavaStreams.java
@@ -2,13 +2,41 @@ package Section14to;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class JavaStreams {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+    public static void regular()
+    {
+
+        ArrayList<String> names=new ArrayList<>();
+        names.add("Bhumika");
+        names.add("Don");
+        names.add("Adam");
+        int count=0;
+
+        for(int i=0; i<names.size(); i++)
+        {
+           String actual= names.get(i);
+           if(actual.startsWith("B"))
+           {
+               count++;
+           }
+        }
+        System.out.println(count);
+    }
+    @Test
+    public void StreamFilter()
+    {
+        ArrayList<String> names=new ArrayList<>();
+        names.add("Bhumika");
+        names.add("Don");
+        names.add("Adam");
+        names.add("Diana");
+
+        Long c=names.stream().filter(s->s.startsWith("D")).count();
+        System.out.println(c);
     }
 }
