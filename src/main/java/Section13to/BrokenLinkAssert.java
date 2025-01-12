105ef033ddd5b8b98138f540013875c275182227
Bhumika Shrestha <66097955+Bhumicha@users.noreply.github.com>
2025-01-12 19:14:50 +0545

    broeklinkupdate

--- src/main/java/Section13to/BrokenLinkAssert.java
+++ src/main/java/Section13to/BrokenLinkAssert.java
@@ -5,21 +5,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
-public class CheckHTTPStatusCode {
+public class BrokenLinkAssert {
     public static void main(String[] args) throws InterruptedException, IOException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://d.kcmg.nl/");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-        List<WebElement> links=   driver.findElements(By.cssSelector("li[class='gf-li'] a"));
-        for(WebElement link : links)
-        {
+        List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+        SoftAssert a = new SoftAssert();
+        for (WebElement link : links) {
             String url = link.getAttribute("href");
 
             HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
@@ -27,10 +28,8 @@ public class CheckHTTPStatusCode {
             conn.connect();
             int resCode = conn.getResponseCode();
             System.out.println(resCode);
-            Assert.assertTrue(resCode<400,"The links with text"+link.getText()+"is broken with code"+resCode);
-            if (resCode > 400) {
-                System.out.println("The link with test" + link.getText()+"is broken with code"+resCode);
-            }
+            a.assertTrue(resCode < 400, "The link with Text" + link.getText() + " is broken with code" + resCode);
         }
+        a.assertAll();
     }
 }
